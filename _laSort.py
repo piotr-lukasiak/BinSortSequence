c8fa1b421904c34332652002ad182b67fb5456e5
Piotr Lukasiak <piotr.lukasiak@primeline.ie>
2024-05-28 16:46:28 +0100

    U-pick implementation. Improved sorting robustness.

--- _laSort.py
+++ _laSort.py
@@ -1,20 +1,44 @@
 def AisleSorting(SortStyle, Aisle1, Aisle2):
     SortedAisle = []
     if 'AisleZig' in SortStyle: 
-        while Aisle1.__len__() > 0:
-            SortedAisle.append(str(Aisle1[0])+"."+SortStyle)
-            SortedAisle.append(str(Aisle2[0])+"."+SortStyle)
-            del Aisle1[0]
-            del Aisle2[0]
+        while len(Aisle1)+len(Aisle2) > 0:
+            if len(Aisle1) > 0:            
+                SortedAisle.append(str(Aisle1[0])+"."+SortStyle)
+                del Aisle1[0]
+            if len(Aisle2) > 0:     
+                SortedAisle.append(str(Aisle2[0])+"."+SortStyle)
+                del Aisle2[0]
+            continue
+    if 'AisleReverseZig' in SortStyle: 
+        while len(Aisle1)+len(Aisle2) > 0:
+            if len(Aisle2) > 0:   
+                SortedAisle.append(str(Aisle2[-1])+"."+SortStyle)
+                del Aisle2[-1]
+            if len(Aisle1) > 0: 
+                SortedAisle.append(str(Aisle1[-1])+"."+SortStyle)
+                del Aisle1[-1]
             continue
 
-    if 'AisleReverseZig' in SortStyle: 
-        while Aisle1.__len__() > 0:
-            SortedAisle.append(str(Aisle2[-1])+"."+SortStyle)
-            SortedAisle.append(str(Aisle1[-1])+"."+SortStyle)
-            del Aisle1[-1]
-            del Aisle2[-1]
-            continue
+    if 'AisleUPick' in SortStyle:
+        while len(Aisle1)+len(Aisle2) > 0:
+            for x in range(2,int(SortStyle[-1])+1):
+                if len(Aisle1) > 0:
+                    SortedAisle.append(str(Aisle1[0]+"."+SortStyle))
+                    del Aisle1[0]
+            for x in range(2,int(SortStyle[-1])+1):
+                if len(Aisle2) > 0:
+                    SortedAisle.append(str(Aisle2[0]+"."+SortStyle))
+                    del Aisle2[0]
+    if 'AisleReverseUpick' in SortStyle:
+        while len(Aisle1)+len(Aisle2) > 0:
+            for x in range(2,int(SortStyle[-1])+1):
+                SortedAisle.append(str(Aisle1[-1])+"."+SortStyle)
+                del Aisle1[-1]
+            for x in range(2,int(SortStyle[-1])+1):
+                SortedAisle.append(str(Aisle2[-1])+"."+SortStyle)
+                del Aisle2[-1]
+
+
     return [str(x).split(".") for x in SortedAisle]
 
 
